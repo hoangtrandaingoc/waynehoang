d13f9a0e0a63f000fca808d70a61b8cd8b0103ee
Wayne <hoangtrandaingoc@gmail.com>
2020-03-03 12:28:00 +0700

    update

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ function order(x) {
 
 order(x);
 x.addListener(order);
+
+
+
+
+//animation for list skill
+
+const skill = document.getElementById("skill");
+let current = true;
+const crt_skill = skill.offsetTop -300;
+
+window.addEventListener('scroll',function(){
+  if(window.pageYOffset > crt_skill){
+	if(current == true){
+	  current = false;
+	  skill.classList.add("myAnimation")
+	}
+  }
+  else if(window.pageYOffset < crt_skill ){
+	if(current == false){
+	  current = true;
+	  skill.classList.remove('myAnimation')
+	}
+  }
+})
