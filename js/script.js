ece3da8dc81a523be0984dbbe9ca63f64918f6ef
Wayne <hoangtrandaingoc@gmail.com>
2020-03-04 17:18:56 +0700

    update

--- js/script.js
+++ js/script.js
@@ -39,3 +39,22 @@ window.addEventListener('scroll',function(){
 	}
   }
 })
+
+
+
+// toggle icon
+
+const icon1 = document.getElementById('icon1');
+const icon2 = document.getElementById('icon2');
+console.log(icon2);
+
+icon1.onclick = function(){
+    icon1.classList.add('hidden_icon1');
+    icon2.classList.add('block_icon2')
+}
+
+icon2.onclick = function(){
+    icon2.classList.remove('block_icon2');
+    icon1.classList.remove('hidden_icon1');
+
+}
